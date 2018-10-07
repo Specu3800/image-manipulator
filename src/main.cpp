5cf9cfc5684d953f41137ab3e46a67f728f9f519
Michał Suliborski <msuliborski@gmail.com>
2018-10-07 23:46:04 +0200

    --help update

--- src/main.cpp
+++ src/main.cpp
@@ -17,84 +17,83 @@ int main(int argc, char* argv[]) {
     CImg<unsigned char> image("../img/color-24bit/lenac.bmp");
     image.save("../out/original.bmp");
 
-    if (argc < 2 || argc > 5) {
-        cout << "Wrong number of parameters. Type --help to view the list of the available commands." << endl;
-        cout << "exit" << endl;
+    if (argc < 2 || argc > 5) {cout << "Wrong number of parameters.\nType --help to view the list of the available commands." << endl; exit(0);
     } else {
         if (argv[1] == string("--help")) displayHelp();
         else if (argv[1] == string("--brightness")) changeBrightness(image, argv[2], argv[4]);
         else if (argv[1] == string("--contrast")) changeContrast(image, argv[2], argv[4]);
         else if (argv[1] == string("--negative")) changeToNegative(image, argv[3]);
-        else {cout << "No maching command. Type --help to view the list of the available commands."; exit(0);}
+        else {cout << "No maching command. \nType --help to view the list of the available commands."; exit(0);}
     }
 }
 
 void displayHelp() {
-    cout << "#############################################################################\n"
-            "########################---   Image Manipulator   ---########################\n"
-            "#############################################################################\n"
-            "#                                                                           #\n"
-            "#                         Michał Suliborski  217863                         #\n"
-            "#                         Arek Zasina        217868                         #\n"
-            "#                                                                           #\n"
-            "#                            Politechnika Łódzka                            #\n"
-            "#                                2018/2019                                  #\n"
-            "#                                                                           #\n"
-            "#############################################################################\n"
-            "\n"
-            << (char)27 << "[1m" << "USAGE:\n" << (char)27 << "[0m" <<
-            "   image-manipulator --command [-argument=value [...]]\n"
-            "\n"
-            << (char)27 << "[1m" << "COMMANDS:\n" << (char)27 << "[0m" <<
-            "   --brightness mod srcPth destPth\n"
-            "       modifies brightess of image in srcPth by a modificator(-255 <= mod <= 255) value and saves image in destPth\n"
-            "\n"
-            "   --contrast mod srcPth destPth\n"
-            "       modifies contrast of image in srcPth by a modificator(0 < mod <= 2) value and saves image in destPth\n"
-            "\n"
-            "	--negative srcPth destPth\n"
-            "		outputs negative of image in srcPth to destPth\n"
-            "\n"
-            "	--hflip srcPth destPth\n"
-            "		outputs horizontally fliped image in srcPth to destPth\n"
-            "\n"
-            "	--vflip srcPth destPth\n"
-            "		outputs vertically fliped image in srcPth to destPth\n"
-            "\n"
-            "	--dflip srcPth destPth\n"
-            "		outputs diagonally fliped image in srcPth to destPth\n"
-            "\n"
-            "	--shrik srcPth destPth\n"
-            "		outputs image in srcPth shrinked by a factor of 2 to destPth\n"
-            "\n"
-            "	--enlarge srcPth destPth\n"
-            "		outputs image in srcPth enlarged by a factor of 2 to destPth\n"
-            "\n"
-            "	--adaptive srcPth destPth\n"
-            "		outputs image in srcPth with adaptive filter applied to destPth\n"
-            "\n"
-            "	--min srcPth destPth\n"
-            "		outputs image in srcPth with minimum filter applied to destPth\n"
-            "\n"
-            "	--max srcPth destPth\n"
-            "		outputs image in srcPth with maximum filter applied to destPth\n"
-            "\n"
-            "	--mse srcPth1 srcPth2\n"
-            "		analyses and displays mean square error between images in srcPth1 and srcPth2\n"
-            "\n"
-            "	--pmse srcPth1 srcPth2\n"
-            "		analyses and displays peak mean square error between images in srcPth1 and srcPth2\n"
-            "\n"
-            "	--snr srcPth1 srcPth2\n"
-            "		analyses and displays signal to noise ratio between images in srcPth1 and srcPth2\n"
-            "\n"
-            "	--psnr srcPth1 srcPth2\n"
-            "		analyses and displays peak signal to noise ratio between images in srcPth1 and srcPth2\n"
-            "\n"
-            "	--md srcPth1 srcPth2\n"
-            "		analyses and displays maximum difference between images in srcPth1 and srcPth2\n"
-            "\n";
-
+    cout <<
+    "###############################################################################\n"
+    "#########################---   " << (char)27 << "[1m" << "Image Manipulator" << (char)27 << "[0m" << "   ---#########################\n"
+    "###############################################################################\n"
+    "#                                                                             #\n"
+    "#                          Michał Suliborski  217863                          #\n"
+    "#                          Arek Zasina        217868                          #\n"
+    "#                                                                             #\n"
+    "#                             Politechnika Łódzka                             #\n"
+    "#                                 2018/2019                                   #\n"
+    "#                                                                             #\n"
+    "###############################################################################\n"
+    "\n"
+    << (char)27 << "[1m" << "USAGE:\n" << (char)27 << "[0m" <<
+    "   image-manipulator --command [-argument=value [...]]\n"
+    "\n"
+    << (char)27 << "[1m" << "COMMANDS:\n" << (char)27 << "[0m" <<
+    "   --brightness mod src dest\n"
+    "       modifies brightess of image in src by a modificator(-255 <= mod <= 255)\n"
+    "       value and saves image in dest\n"
+    "\n"
+    "   --contrast mod src dest\n"
+    "       modifies contrast of image in src by a modificator(0 < mod <= 2)\n"
+    "       value and saves image in dest\n"
+    "\n"
+    "   --negative src dest\n"
+    "      outputs negative of image in src to dest\n"
+    "\n"
+    "   --hflip src dest\n"
+    "      outputs horizontally fliped image in src to dest\n"
+    "\n"
+    "   --vflip src dest\n"
+    "      outputs vertically fliped image in src to dest\n"
+    "\n"
+    "   --dflip src dest\n"
+    "      outputs diagonally fliped image in src to dest\n"
+    "\n"
+    "   --shrik src dest\n"
+    "      outputs image in src shrinked by a factor of 2 to dest\n"
+    "\n"
+    "   --enlarge src dest\n"
+    "      outputs image in src enlarged by a factor of 2 to dest\n"
+    "\n"
+    "   --adaptive src dest\n"
+    "      outputs image in src with adaptive filter applied to dest\n"
+    "\n"
+    "   --min src dest\n"
+    "      outputs image in src with minimum filter applied to dest\n"
+    "\n"
+    "   --max src dest\n"
+    "      outputs image in src with maximum filter applied to dest\n"
+    "\n"
+    "   --mse src1 src2\n"
+    "      displays mean square error between images in src1 and src2\n"
+    "\n"
+    "   --pmse src1 src2\n"
+    "      displays peak mean square error between images in src1 and src2\n"
+    "\n"
+    "   --snr src1 src2\n"
+    "      displays signal to noise ratio between images in src1 and src2\n"
+    "\n"
+    "   --psnr src1 src2\n"
+    "      displays peak signal to noise ratio between images in src1 and src2\n"
+    "\n"
+    "   --md src1 src2\n"
+    "      displays maximum difference between images in src1 and src2\n";
     exit(0);
 }
 
@@ -116,8 +115,7 @@ void changeBrightness(CImg<unsigned char> img, char* val, char* path) {
 
 void changeContrast(CImg<unsigned char> img, char* val, char* path) {
     float value = atof(val);
-    if (value < 0) {cout << "Wrong value of the contrast - value should be a positive number"; exit(0);}
-
+    if (value < 0) {cout << "Wrong value of the contrast. \nType --help to view information about available commands."; exit(0);}
     cout << "Changing contrast by " << value << " of image: " << path << endl;
     float beta = 128 - 128 * value;
     for (int x = 0; x < img.width(); x++) {
