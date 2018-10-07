2efa550eb20157e4d4aa8a7a6837d0d9a2ce7a3d
Michał Suliborski <msuliborski@gmail.com>
2018-10-07 14:20:47 +0200

    simple menu

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,55 @@
 
 using namespace std;
 
+void displayHelp();
+void changeBrightness(char*, char*);
+void changeContrast(char*, char*);
+void changeToNegative(char*);
+void exitWithError(int);
+
+void displayHelp() {
+    cout << "really nice help message" << endl;
+    exit(0);
+}
+
+void changeBrightness(char* val, char* path) {
+    cout << "changing brightness to " << val << " of image: " << path << endl;
+    exit(0);
+}
+
+void changeContrast(char* val, char* path) {
+    cout << "changing contrast to " << val << " of image: " << path << endl;
+    exit(0);
+}
+
+void changeToNegative(char* path) {
+    cout << "changing to negative image: " << path << endl;
+    exit(0);
+}
+
+void exitWithError(int e){
+    switch(e){
+        case 1:
+            cout << "no maching command, use --help for more" << endl;
+            break;
+        default:
+            cout << "error" << endl;
+            break;
+    }
+}
+
+
 int main(int argc, char* argv[]) {
 
     if (argc < 2) {
         cout << "no args" << endl;
         cout << "exit" << endl;
     } else {
-
-        if (strcmp(argv[1], "--help")){
-            cout << "really nice help message" << endl;
-            cout << "exit" << endl;
-        } else if (strcmp(argv[1], "--vflip") != 0){
-            cout << "really nice vflip" << endl;
-            cout << argv[1] << endl;
-            cout << "exit" << endl;
-        } else if (strcmp(argv[1], "--hflip") != 0) {
-            cout << "really nice hflip" << endl;
-            cout << "exit" << endl;
-        } else {
-            cout << "no maching command" << endl;
-            cout << "exit" << endl;
-        }
+        if (argv[1] == string("--help")) displayHelp();
+        else if (argv[1] == string("--brightness")) changeBrightness(argv[2], argv[3]);
+        else if (argv[1] == string("--contrast")) changeContrast(argv[2], argv[3]);
+        else if (argv[1] == string("--negative")) changeToNegative(argv[2]);
+        else exitWithError(1);
     }
-
-
 }
+
