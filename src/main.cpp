4810dbb56b8cb687942f554f63fef1a3b70f1d54
Michał Suliborski <msuliborski@gmail.com>
2018-10-07 13:55:15 +0200

    clean-up 1/2

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    cout << "Hello, World!" << endl;
-    return 0;
+
+    if (argc < 2) {
+        cout << "no args" << endl;
+        cout << "exit" << endl;
+    } else {
+
+        if (strcmp(argv[1], "--help")){
+            cout << "really nice help message" << endl;
+            cout << "exit" << endl;
+        } else if (strcmp(argv[1], "--vflip") != 0){
+            cout << "really nice vflip" << endl;
+            cout << argv[1] << endl;
+            cout << "exit" << endl;
+        } else if (strcmp(argv[1], "--hflip") != 0) {
+            cout << "really nice hflip" << endl;
+            cout << "exit" << endl;
+        } else {
+            cout << "no maching command" << endl;
+            cout << "exit" << endl;
+        }
+    }
+
+
 }
