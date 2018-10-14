36f781e8e5b46bfecde2855d186d9fecd6461df5
Michał Suliborski <msuliborski@gmail.com>
2018-10-14 19:45:42 +0200

    dflip + refacotr of img

--- src/manipulations/t0-other.cpp
+++ src/manipulations/t0-other.cpp
@@ -11,15 +11,20 @@ CImg<int> applyBilinearInterpolation(CImg<int> img){
 
             if(x-1 < 0 || x + 1 >= img.width() || y-1 < 0 || y + 1 >= img.height()) continue; //not for borders
             if(img(x, y, 0, 0) != 0 || img(x, y, 0, 1) != 0 || img(x, y, 0, 2) != 0) continue; //apply only on black pixels
-            if(img(x+1, y+1, 0, 0) == 0 && img(x+1, y+1, 0, 1) == 0 && img(x+1, y+1, 0, 2) == 0) continue; //if surrounded by black pixels
-            if(img(x+1, y-1, 0, 0) == 0 && img(x+1, y-1, 0, 1) == 0 && img(x+1, y-1, 0, 2) == 0) continue;
-            if(img(x-1, y+1, 0, 0) == 0 && img(x-1, y+1, 0, 1) == 0 && img(x-1, y+1, 0, 2) == 0) continue;
-            if(img(x-1, y-1, 0, 0) == 0 && img(x-1, y-1, 0, 1) == 0 && img(x-1, y-1, 0, 2) == 0) continue;
+
             for (int j = 0; j < 3; j++) {
-                img(x, y-1, 0, j) = 0.5*img(x-1, y-1, 0, j) + 0.5*img(x+1, y-1, 0, j);
-                img(x, y+1, 0, j) = 0.5*img(x-1, y+1, 0, j) + 0.5*img(x+1, y+1, 0, j);
-                img(x+1, y, 0, j) = 0.5*img(x+1, y-1, 0, j) + 0.5*img(x+1, y+1, 0, j);
-                img(x-1, y, 0, j) = 0.5*img(x-1, y+1, 0, j) + 0.5*img(x-1, y-1, 0, j);
+                if  (img(x-1, y-1, 0, 0) != 0 && img(x-1, y-1, 0, 1) != 0 && img(x-1, y-1, 0, 2) != 0) {
+                    if (img(x + 1, y - 1, 0, 0) != 0 && img(x + 1, y - 1, 0, 1) != 0 && img(x + 1, y - 1, 0, 2) != 0)
+                        img(x, y - 1, 0, j) = 0.5 * img(x - 1, y - 1, 0, j) + 0.5 * img(x + 1, y - 1, 0, j);
+                    if (img(x - 1, y + 1, 0, 0) != 0 && img(x - 1, y + 1, 0, 1) != 0 && img(x - 1, y + 1, 0, 2) != 0)
+                        img(x - 1, y, 0, j) = 0.5 * img(x - 1, y + 1, 0, j) + 0.5 * img(x - 1, y - 1, 0, j);
+                }
+                if  (img(x+1, y+1, 0, 0) != 0 && img(x+1, y+1, 0, 1) != 0 && img(x+1, y+1, 0, 2) != 0) {
+                    if (img(x + 1, y - 1, 0, 0) != 0 && img(x + 1, y - 1, 0, 1) != 0 && img(x + 1, y - 1, 0, 2) != 0)
+                        img(x+1, y, 0, j) = 0.5*img(x+1, y-1, 0, j) + 0.5*img(x+1, y+1, 0, j);
+                    if (img(x - 1, y + 1, 0, 0) != 0 && img(x - 1, y + 1, 0, 1) != 0 && img(x - 1, y + 1, 0, 2) != 0)
+                        img(x, y+1, 0, j) = 0.5*img(x-1, y+1, 0, j) + 0.5*img(x+1, y+1, 0, j);
+                }
                 img(x, y, 0, j) = 0.5*img(x, y-1, 0, j) + 0.5*img(x, y+1, 0, j);
             }
         }
