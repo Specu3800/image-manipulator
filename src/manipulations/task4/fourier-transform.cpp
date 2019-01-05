a56e2fa7141058a5c8f3d60ca2b5e236bd67a7f5
Michał Suliborski <msuliborski@gmail.com>
2019-01-05 21:38:05 +0100

    fourier done

--- src/manipulations/task4/fourier-transform.cpp
+++ src/manipulations/task4/fourier-transform.cpp
@@ -12,28 +12,27 @@ using namespace std;
 using namespace cimg_library;
 
 vector<vector<complex<double>>>& applyDFT(CImg<int> &original){
-
     auto* output = new vector<vector<complex<double>>>;
     vector<vector<complex<double>>> temp;
 
-    for (int y = 0; y < original.height(); y++){ //through each row
+    for (int y = 0; y < original.height(); y++){
         vector<complex<double>> row;
-        temp.push_back(row); //add empty row
-        for (int x = 0; x < original.width(); x++){ //for every value of that row...
-            complex<double> sum = (0.0, 0.0); //...get the sum...
-            for (int xx = 0; xx < original.width(); xx++){ //...of every other number in that row...
-                complex<double> comp(cos(2 * M_PI * xx * x / original.width()), -sin(2 * M_PI * xx * x / original.width())); //...put in this equation
+        temp.push_back(row);
+        for (int x = 0; x < original.width(); x++){
+            complex<double> sum = (0.0, 0.0);
+            for (int xx = 0; xx < original.width(); xx++){
+                complex<double> comp(cos(2 * M_PI * xx * x / original.width()), -sin(2 * M_PI * xx * x / original.width()));
                 sum += (double)(original(xx, y)) * comp;
             }
-            temp[y].push_back(sum); //and put it in temporary 2d array
+            temp[y].push_back(sum);
         }
-    }// so we have 2d array of comp nums and every number is calculated from numbers of its row
+    }
 
-    for (int x = 0; x < original.width(); x++){ //now go through each column
+    for (int x = 0; x < original.width(); x++){
         vector<complex<double>> col;
-        (*output).push_back(col); //prepare space
-        for (int y = 0; y < original.height(); y++){ //for every value of that column...
-            complex<double> sum = (0.0, 0.0); //...get the sum...
+        (*output).push_back(col);
+        for (int y = 0; y < original.height(); y++){
+            complex<double> sum = (0.0, 0.0);
             for (int yy = 0; yy < original.height(); yy++){
                 complex<double> comp(cos(2 * M_PI * yy * y / original.height()), -sin(2 * M_PI * yy * y / original.height()));
                 sum += temp[yy][x] * comp;
@@ -41,7 +40,6 @@ vector<vector<complex<double>>>& applyDFT(CImg<int> &original){
             (*output)[x].push_back(sum);
         }
     }
-
     return *output;
 }
 
@@ -81,60 +79,93 @@ CImg<int>& applyIDFT(vector<vector<complex<double>>> &original){
 }
 
 vector<vector<complex<double>>>& applyFFT(CImg<int> &original){
-//    auto* output = new vector<vector<complex<double>>>;
-//    vector<vector<complex<double>>> temp;
-//
-//    cout << "wad" << endl; cout.flush();
-//
-//    for (int y = 0; y < original.height(); y++){
-//        vector<complex<double>> row(original.width());
-//        temp.push_back(row);
-//        for (int x = 0; x < original.width() / 2; x++){
-//            complex<double> sum1 = (0., 0.);
-//            complex<double> sum2 = (0., 0.);
-//            for (int xx = 0; xx < original.width() / 2; xx++){
-//                complex<double> comp1(cos(2 * M_PI * xx * x / (original.width() / 2)), -sin(2 * M_PI * xx * x / (original.width() / 2)));
-//                complex<double> comp2(cos(2 * M_PI * xx * x / (original.width() / 2)), -sin(2 * M_PI * xx * x / (original.width() / 2)));
-//                sum1 += (double)original(2 * xx, y) * comp1;
-//                sum2 += (double)original(2 * xx + 1, y) * comp2;
-//            }
-//            complex<double> W(cos(2 * M_PI * x / original.width()), -sin(2 * M_PI * x / original.width()));
-//            temp[y][x] = sum1 + (sum2 * W);
-//            temp[y][original.width() / 2 + x] = sum1 - (sum2 * W);
-//        }
-//    }
-//
-//    cout << "wad2" << endl; cout.flush();
-//
-//    for (int x = 0; x < original.width(); x++){
-//        vector<complex<double>> row(original.height());
-//        (*output).push_back(row);
-//        for (int y = 0; y < original.height() / 2; y++){
-//            complex<double> sum1 = (0., 0.);
-//            complex<double> sum2 = (0., 0.);
-//            for (int yy = 0; yy < original.height() / 2; yy++){
-//                complex<double> comp1(cos(2 * M_PI * yy * y / (original.height() / 2)), -sin(2 * M_PI * yy * y / (original.height() / 2)));
-//                complex<double> comp2(cos(2 * M_PI * yy * y / (original.height() / 2)), -sin(2 * M_PI * yy * y / (original.height() / 2)));
-//                sum1 += (*output)[2 * yy][x] * comp1;
-//                sum2 += temp[2 * yy + 1][x] * comp2;
-//                cout << "wad2.5" << endl; cout.flush();
-//            }
-//            cout << "wad3" << endl; cout.flush();
-//            complex<double> W(cos(2 * M_PI * y / original.width()), -sin(2 * M_PI * y / original.width()));
-//            cout << "wad4" << endl; cout.flush();
-//            (*output)[x][y] = sum1 + (sum2 * W);
-//            cout << "wad4" << endl; cout.flush();
-//            (*output)[x][original.height() / 2 + y] = sum1 - (sum2 * W);
-//        }
-//    }
-//
-//    return *output;
+    auto* output = new vector<vector<complex<double>>>;
+    vector<vector<complex<double>>> temp;
+
+    for (int y = 0; y < original.height(); y++){
+        vector<complex<double>> row(original.width());
+        temp.push_back(row);
+        for (int x = 0; x < original.width() / 2; x++){
+            complex<double> sum1 = (0., 0.);
+            complex<double> sum2 = (0., 0.);
+            for (int xx = 0; xx < original.width() / 2; xx++){ //same as before, but we split numbers for odd and even
+                complex<double> comp1(cos(2 * M_PI * xx * x / (original.width() / 2)), -sin(2 * M_PI * xx * x / (original.width() / 2)));
+                complex<double> comp2(cos(2 * M_PI * xx * x / (original.width() / 2)), -sin(2 * M_PI * xx * x / (original.width() / 2)));
+                sum1 += (double)original(2 * xx, y) * comp1;
+                sum2 += (double)original(2 * xx + 1, y) * comp2;
+            }
+            complex<double> W(cos(2 * M_PI * x / original.width()), -sin(2 * M_PI * x / original.width()));
+            temp[y][x] = sum1 + (sum2 * W);
+            temp[y][original.width() / 2 + x] = sum1 - (sum2 * W);
+        }
+    }
+
+    for (int x = 0; x < original.width(); x++){
+        vector<complex<double>> row(original.height());
+        (*output).push_back(row);
+        for (int y = 0; y < original.height() / 2; y++){
+            complex<double> sum1 = (0., 0.);
+            complex<double> sum2 = (0., 0.);
+            for (int yy = 0; yy < original.height() / 2; yy++){
+                complex<double> comp1(cos(2 * M_PI * yy * y / (original.height() / 2)), -sin(2 * M_PI * yy * y / (original.height() / 2)));
+                complex<double> comp2(cos(2 * M_PI * yy * y / (original.height() / 2)), -sin(2 * M_PI * yy * y / (original.height() / 2)));
+                sum1 += temp[2 * yy][x] * comp1;
+                sum2 += temp[2 * yy + 1][x] * comp2;
+            }
+            complex<double> W(cos(2 * M_PI * y / original.width()), -sin(2 * M_PI * y / original.width()));
+            (*output)[x][y] = sum1 + (sum2 * W);
+            (*output)[x][original.height() / 2 + y] = sum1 - (sum2 * W);
+        }
+    }
+
+    return *output;
 }
 
 
 CImg<int>& applyIFFT(vector<vector<complex<double>>> &original){
     CImg<int>* output = new CImg<int>(original[0].size(), original.size(), 1, 3, 0);
+    vector<vector<complex<double>>> temp;
 
+    for (int i = 0; i < (*output).width(); i++) {
+        vector<complex<double>> row((*output).width());
+        temp.push_back(row);
+    }
+
+    for (int x = 0; x < (*output).width(); x++){
+
+        for (int y = 0; y < (*output).height() / 2; y++){
+            complex<double> sum1 = (0., 0.);
+            complex<double> sum2 = (0., 0.);
+            for (int yy = 0; yy < (*output).height() / 2; yy++){
+                complex<double> comp1(cos(2 * M_PI * yy * y / ((*output).height() / 2)), sin(2 * M_PI * yy * y / ((*output).height() / 2)));
+                complex<double> comp2(cos(2 * M_PI * yy * y / ((*output).height() / 2)), sin(2 * M_PI * yy * y / ((*output).height() / 2)));
+                sum1 += original[2 * yy][x] * comp1;
+                sum2 += original[2 * yy + 1][x] * comp2;
+            }
+            complex<double> W(cos(2 * M_PI * y / (*output).width()), sin(2 * M_PI * y / (*output).width()));
+
+            temp[y][x] = (sum1 + (sum2 * W)) / (double) ((*output).height());
+            temp[(*output).height()/2 + y][x] = (sum1 - (sum2 * W)) / (double) ((*output).height());
+        }
+    }
+
+    for (int y = 0; y < (*output).height(); y++){
+        for (int x = 0; x < (*output).width() / 2; x++){
+            complex<double> sum1 = (0., 0.);
+            complex<double> sum2 = (0., 0.);
+            for (int xx = 0; xx < (*output).width() / 2; xx++){
+                complex<double> comp1(cos(2 * M_PI * xx * x / ((*output).width() / 2)), sin(2 * M_PI * xx * x / ((*output).width() / 2)));
+                complex<double> comp2(cos(2 * M_PI * xx * x / ((*output).width() / 2)), sin(2 * M_PI * xx * x / ((*output).width() / 2)));
+                sum1 += temp[y][2 * xx] * comp1;
+                sum2 += temp[y][2 * xx + 1] * comp2;
+            }
+            complex<double> W(cos(2 * M_PI * x / (*output).width()), sin(2 * M_PI * x / (*output).width()));
+            for (int c = 0; c < (*output).spectrum(); c++){
+                (*output)(y, x, c) = abs(sum1 + (sum2 * W)) / (*output).width();
+                (*output)(y, (*output).width()/2 + x, c) = abs(sum1 - (sum2 * W)) / (*output).width();
+            }
+        }
+    }
     return *output;
 }
 
@@ -162,8 +193,8 @@ vector<vector<complex<double>>>& swapQuarters(vector<vector<complex<double>>> &o
 
 
 CImg<int>& getFourierImage(vector<vector<complex<double>>> &original){
-
     CImg<int>* output = new CImg<int>(original[0].size(), original.size(), 1, 3, 0);
+
     for (int x = 0; x < original[0].size(); x++) {
         for (int y = 0; y < original.size(); y++) {
             for (int c = 0; c < 3; c++) {
